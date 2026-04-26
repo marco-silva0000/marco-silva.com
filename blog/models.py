b8f33a755f7fcf5474b8335824d32e231a1c2e77
Marco Silva <marco-silva0000@users.noreply.github.com>
2026-04-27 00:54:20 +0300

    replace custom blog with wagtail, add markdown support

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+from wagtail.models import Page
+from wagtail import blocks
+from wagtailmarkdown.blocks import MarkdownBlock
 
-# Create your models here.
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey("BlogPostPage", related_name="tagged_items", on_delete=models.CASCADE)
+
+
+class BlogIndexPage(Page):
+    subpage_types = ["blog.BlogPostPage"]
+    max_count = 1
+
+    def get_context(self, request):
+        ctx = super().get_context(request)
+        ctx["posts"] = BlogPostPage.objects.live().public().order_by("-first_published_at")
+        return ctx
+
+
+class BlogPostPage(Page):
+    body = StreamField([
+        ("markdown", MarkdownBlock()),
+        ("raw_html", blocks.RawHTMLBlock()),
+    ], use_json_field=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+        FieldPanel("tags"),
+    ]
+
+    parent_page_types = ["blog.BlogIndexPage"]
