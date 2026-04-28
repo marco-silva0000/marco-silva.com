c9fd92ed634c4ec9fa5fbda4776fde4db1f3a8c6
Marco Silva <marco-silva0000@users.noreply.github.com>
2026-04-28 16:44:41 +0300

    fix emojinary namespace registration

--- gamerooms/urls.py
+++ gamerooms/urls.py
@@ -7,6 +7,6 @@ app_name = "gamerooms"
 urlpatterns = [
     path("", views.room_list, name="room-list"),
     path("create/", views.room_create, name="room-create"),
-    path("emojinary/", include("emojinary.urls")),
+    path("emojinary/", include(("emojinary.urls", "emojinary"))),
     path("<str:code>/", views.room_join, name="room-join"),
 ]
